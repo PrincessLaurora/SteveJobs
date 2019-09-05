31aeab5cfdfce36f36e15f9bac079c230ae346b7
Laura Felce <crepu83@yahoo.it>
2019-09-05 08:39:15 +0000

    session storage test

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,15 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
     end
   end
+
+
+
+    def self.serialize_into_session(record)
+      [record.to_key.map(&:to_s), record.authenticatable_salt]
+    end
+
+    def self.serialize_into_cookie(record)
+      [record.to_key.map(&:to_s), record.rememberable_value]
+    end
+
 end
